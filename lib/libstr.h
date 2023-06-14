56c2a9d44b2abafe4f8eb2d70a1314337e67c178
hotnuma <hotnuma@gmail.com>
2023-06-14 18:25:02 +0200

    remove str functions

--- lib/libstr.h
+++ lib/libstr.h
@@ -29,7 +29,6 @@ bool str_endswith(const char *str, const char *part, bool sensitive);
 // Browse ---------------------------------------------------------------------
 
 bool str_getlineptr(char **start, char **result, int *length);
-bool str_getpart(char **start, char **result, int *length);
 
 // Std C extensions -----------------------------------------------------------
 
